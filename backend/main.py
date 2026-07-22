e566dc42adebf05632d9a8b37d468ae828c67b0e
prajwal2005singh <prajwal2005singh@gmail.com>
2026-07-22 22:28:46 +0530

    changes in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.projects import router
-import json, os
+
 
 app = FastAPI(title="Promise vs Progress API")
 
